5a5297eff4e7bbe88ba1f7f3870222d324f48335
rvrok <rvrok@DESKTOP-7O5BMEJ>
2021-07-06 17:50:26 +0530

    The following 3 methods have been implemented:- 1) Method:getRandomNumbersForGrid() 2) Method:handleButtonClicked() 3) Method:checkForWinner()

--- NumberPuzzleQuestion/src/CustomNumberPuzzleControl.java
+++ NumberPuzzleQuestion/src/CustomNumberPuzzleControl.java
@@ -1,4 +1,6 @@
 import java.awt.*;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 class CustomNumberPuzzleControl extends NumberPuzzleControl {
 	public int getWidth() {
@@ -34,20 +36,51 @@ class CustomNumberPuzzleControl extends NumberPuzzleControl {
 	public int handleButtonClicked(NumberPuzzleGame game){
 		int emptyCellId = game.getEmptyCellId();
 		Button buttonClicked = game.getButtonClicked();
+		int newEmpty=emptyCellId;
 		Button[] buttons = game.getButtons();
-		
-		//Your logic here		
-		
-		return emptyCellId;
+		for(int i=0;i<16;i++) {
+			if (buttons[i]==buttonClicked)
+				newEmpty=i;
+		}
+		int flag=0;
+		//Your logic here
+//		System.out.println(emptyCellId);	
+//		System.out.println(buttonClicked);	
+//		System.out.println(Arrays.toString(buttons));	
+		//Button emptyButton=new Button();
+		//emptyButton.
+		if(emptyCellId-newEmpty==4||newEmpty-emptyCellId==4||((newEmpty-emptyCellId==1&&(newEmpty/4==emptyCellId/4))||(emptyCellId-newEmpty==1&&(newEmpty/4==emptyCellId/4)))) {
+		swapButton(buttons[emptyCellId],buttonClicked);
+		//emptyCellId=game.getEmptyCellId()
+		flag=1;
+		game.setEmptyCellId(newEmpty);
+		}
+		if(flag==1)
+		return newEmpty;
+		else return emptyCellId;
 
 	}
 	public int[] getRandomNumbersForGrid() {
 		int arr[] = new int[15];
+		ArrayList<Integer> a1=new ArrayList<Integer>();
+		int temp=0;
+		for(int i=0;i<15;i++) {
+			while(true) {
+		temp = getRandomNumber();
+		temp=temp*15/100+1;
 		
-		//Your logic here
-		int a = getRandomNumber();
+		if(!a1.contains(temp)) {
+			
+			a1.add(temp);
+			
+			break;
+		}
+			}
+			//a1.add(i+1);
 		
-		
+		}
+		for(int i=0;i<15;i++)
+			arr[i]=a1.get(i);
 		return arr;
 	}
 	public boolean checkForWinner(Button[] buttons)
@@ -55,8 +88,15 @@ class CustomNumberPuzzleControl extends NumberPuzzleControl {
 		boolean winner = true;
 		
 		// Your Logic here
-		getIntegerArrayOfButtonIds(buttons);
-
+		int b_arr[]=getIntegerArrayOfButtonIds(buttons);
+		for(int i=0;i<15;i++) {
+//			System.out.println(Arrays.toString(b_arr));
+			if(b_arr[i]!=i+1)
+			{	
+				winner=false;
+				break;
+			}
+		}
 		return winner;
 	}
 }
